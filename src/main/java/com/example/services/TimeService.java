30dc73950062ac95a8284f8f813594823619d746
Legand Burge <blegand@gmail.com>
2014-11-11 18:36:29 -0500

    Made first Change

--- src/main/java/com/example/services/TimeService.java
+++ src/main/java/com/example/services/TimeService.java
@@ -1,9 +1,12 @@
 package com.example.services;
 
+import java.util.TimeZone;
+
 import com.example.models.Time;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -16,5 +19,11 @@ public class TimeService {
         return new Time();
     }
 
+    
+    @GET
+    @Path("/{timezone}")
+    public Time get(@PathParam("timezone") String timezone) {
+        return new Time(TimeZone.getTimeZone(timezone.toUpperCase()));
+    }
 }
 
